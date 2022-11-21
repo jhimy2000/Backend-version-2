7af28eed67a8c9850fa4c71c45c19e301ad8b7fa
jhimy2000 <u201818218@upc.edu.pe>
2022-11-21 05:33:55 -0500

    Permisos para el frond v 1

--- src/main/java/com/merkadeam/market/web/controller/ProductController.java
+++ src/main/java/com/merkadeam/market/web/controller/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@CrossOrigin(origins = {"http://localhost:4200","https://frond-merkadea-v2.web.app"})
+@CrossOrigin(origins = {"http://localhost:4200","https://frond-merkadea-v2.web.app","https://merkadea-market.web.app"})
 @RestController
 @RequestMapping("/products")
 public class ProductController {
