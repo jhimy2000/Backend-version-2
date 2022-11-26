fc0c9a6e2ad8a939087d32dfe99d7fb3300e4572
jhimy <u201818218@upc.edu.pe>
2022-11-25 23:26:04 -0500

    puliendo el swagger

--- src/main/java/com/merkadeam/market/MerkadeamMarketApplication.java
+++ src/main/java/com/merkadeam/market/MerkadeamMarketApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Merkadea", version = "2.0", description = "Merkadea Informatio"))
+@OpenAPIDefinition(info = @Info(title = "Merkadea", version = "2.0", description = "Esta documentación presenta las peticiones de get, post, put y delete de nuestras entidades utilizadas en nuestro proyecto Merkadea"))
 public class MerkadeamMarketApplication {
 
 	public static void main(String[] args) {
